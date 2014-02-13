c49a87f34fd1c505860e5fb1a948db63ac58268c
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-02-13 00:39:56 +0000

    Safe logic.

--- src/Bucket.php
+++ src/Bucket.php
@@ -11,7 +11,7 @@ namespace Gajus\Skip;
 class Bucket implements \ArrayAccess {
     private
         /**
-         * @var string $uid Unique ID for the session container.
+         * @var string $uid Unique session container ID.
          */
         $uid,
         /**
@@ -29,33 +29,58 @@ class Bucket implements \ArrayAccess {
         $this->data = isset($_SESSION['gajus']['skip']['bucket'][$uid]) ? $_SESSION['gajus']['skip']['bucket'][$uid] : [];
     }
 
+    /**
+     * @return string Unique session container ID.
+     */
     public function getUid () {
         return $this->uid;
     }
 
+    /**
+     * @param mixed $offset
+     * @return boolean
+     */
     public function offsetExists ($offset) {
         return isset($this->data[$offset]);
     }
 
+    /**
+     * @param mixed $offset
+     * @return mixed
+     */
     public function offsetGet ($offset) {
         return isset($this->data[$offset]) ? $this->data[$offset] : null;
     }
 
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     * @return void
+     */
     public function offsetSet ($offset, $value) {
         $this->data[$offset] = $value;
     }
 
+    /**
+     * @param mixed $offset
+     * @return void
+     */
     public function offsetUnset ($offset) {
         unset($this->data[$offset]);
     }
 
     /**
+     * Bucket data is stored if there is no content displayed.
+     * Bucket data is discarded if there is content displayed.
+     * 
      * @see http://stackoverflow.com/questions/21737903/how-to-get-content-length-at-the-end-of-request#21737991 Detect if body has been sent to the browser.
      */
     public function __destruct () {
         register_shutdown_function(function () {
             if (count(array_filter(ob_get_status(true), function ($status) { return $status['buffer_used']; } ))) {
                 $_SESSION['gajus']['skip']['bucket'][$this->getUid()] = [];
+            } else {
+                $_SESSION['gajus']['skip']['bucket'][$this->getUid()] = $this->data;
             }
         });
     }
